2c6380fa397ed50e10e01e7a6bc53cc994c79e4f
zzjlxzr2012 <1315214906@qq.com>
2022-08-19 17:56:50 +0800

    all

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+const t = 2
+</script>
+
+<template>
+  <div>
+    <router-link to="/myCanvas/exp-A">Go to A</router-link>
+    <router-link to="/myCanvas/exp-B">Go to B</router-link>
+  </div>
+  <div>
+    <keep-alive>
+      <router-view :key="$route.fullPath"></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/MyCanvas.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { onActivated, onDeactivated, onMounted, ref } from 'vue'
+import {useRouter} from 'vue-router'
+
+let router = useRouter()
+let nowRoute = router.currentRoute
+let nowCanvas = nowRoute.value.params.id
+console.log(nowCanvas)
+
+onMounted(() => {
+  var canvas = document.getElementById('tutorial');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    if(nowCanvas=='exp-A'){
+      ctx.fillStyle = "rgb(200,0,0)";
+    }else{
+      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    }
+    ctx.fillRect(30, 30, 55, 50);
+  }
+})
+onActivated(()=>{
+  console.log('激活啦',nowRoute)
+})
+onDeactivated(()=>{
+  console.log('出去啦',nowRoute)
+})
+
+</script>
+
+<template>
+  <h1>{{nowRoute.params.id}}</h1>
+  <canvas id="tutorial" width="150" height="150"></canvas>
+</template>
+
+<style scoped>
+</style>
